d9e2b393c556db716be19a3c22ab8b051a465adc
lczch <rice-wine@foxmail.com>
2021-01-25 16:24:22 +0800

    fix

--- main.js
+++ main.js
@@ -76,6 +76,6 @@ var server = http.createServer(function (request, response) {
 });
 
 
-server.listen(8080);
+server.listen(80);
 
-console.log('Server is running at http://127.0.0.1:8080/');
+// console.log('Server is running at http://127.0.0.1:8080/');
